b3ecaa435d16a191117bb61c3274ddab7409f875
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2020-01-23 13:37:48 +0700

    :zap: Finishing Main Page + Responsive

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,36 @@
 import React from "react"
 import main from "../assets/img/main.png"
-
+import bubble from "../assets/img/bubble.png"
 
 const Main = () => (
-  <div className="bannerHeader">
-    {/* <div className="d-flex flex-row">
-      <div className="d-flex flex-column text-white ml-5 mt-3">
-        <div className="bubble-pic text-center ">         
-          <h1 className="bubble-text text-dark m-auto">Hi, Im</h1>
-        </div>
+  <div className="main">
 
+    <div className="flex lg:flex-row flex-col h-full">
+
+      <div className="lg:-mt-40 lg:w-1/2 sm:w-auto text-white text-center self-center ">
+
+        <img src={bubble} alt="bubble-icon" className="bubble-pic mr-auto"/>
+        <h1 className="myName mr-auto ml-auto text-4xl font-black ">Ahmat Dani Setiawan</h1>
+        <br/>
+
+        <span className="devBig">W</span><span className="lg:text-5xl font-black">eb</span>       
+        &nbsp;&nbsp;
+        <span className="devBig">D</span><span className="lg:text-4xl font-black">eveloper</span>
        
+        
+        {/* <h2>Web <br/> & <br/> Mobile </h2>     
+        <h2> Developer</h2> */}
       </div>
-      <div className="ml-auto mt-3 ">
-        <img src={main} alt="Main" className="main-pic" />
+
+      <div className="lg:w-1/2 sm:w-auto self-center ">
+        <img src={main} alt="Main" className="main-pic object-cover mb-10 " />
       </div>
-    </div> */}
+
+    </div>
+
+
   </div>
-  
+
 )
 
 export default Main;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Skills = () => (
-    <div className="bannerHeader">
-      <div className="d-flex flex-row">
-        <div className="d-flex flex-column text-white ml-5 mt-3">
+    <div className="skills">
+      <div className="flex-row">
+        <div className="flex-column text-white ml-5 mt-3">
           <h1>Skills Page</h1>        
         </div>
         {/* <img src="">
